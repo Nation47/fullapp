092f59d64ee3e52e20e619b51371b5b7c35941f5
Nation47 <hananiyusuph98@gmail.com>
2023-12-03 23:24:10 +0300

    functionality

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,10 +1,26 @@
+import { useWorkoutsContext } from "../hooks/useContext"
+
 const WorkoutDetails = ({ workout }) => {
+    const { dispatch } = useWorkoutsContext()
+
+    const handleClick = async () => {
+        const response = await fetch('/api/workouts' + workout._id, {
+            method: 'DELETE_WORKOUT'
+        })
+
+        const json = await response.json()
+
+        if(response.ok) {
+            dispatch({type: 'DELETE_WORKOUT', payload: json})
+        }
+    }
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong>load (kg): </strong>{workout.load}</p>
             <p><strong>reps: </strong>{workout.reps}</p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>Delete</span>
         </div>
     );
 }
